f0bdc2482296dd23a31a1202c58220cde7aa29a9
Robin Lovelace <rob00x@gmail.com>
2021-08-23 08:09:27 +0100

    Update with R functions

---
+++ R/get.R
@@ -0,0 +1,90 @@
+#' Get OSM data associated with a particular type of infrastructure
+#'
+#' @param place Description of the geographical area that should be matched
+#'   with a .osm.pbf file
+#' @param infra_type Type of infrastructure, e.g. cycle_infrastructure
+#' @param query A character string representing an SQL query to send to
+#'   `oe_get()`
+#' @param extra_tags Which additional columns, corresponding to OSM tags,
+#'   should be in the resulting dataset?
+#' @param ... Arguments passed to `oe_get()`
+#' @import sf
+#' @export
+#' @examples
+#' highways = oi_get("isle of wight", infra_type = "highway")
+#' cycleways = oi_get("isle of wight", infra_type = "cycle_infrastructure")
+#' plot(cycleways)
+#' cycleways_minimal = oi_get("isle of wight", infra_type = "cycle_infrastructure_minimal")
+oi_get = function(place, infra_type = NULL, query = NULL, extra_tags = et, ...) {
+  if(is.null(infra_type) && is.null(query)) {
+    stop("No infra_type or query provided")
+  }
+  if(is.null(query)) {
+    query = oi_query(infra_type)
+  }
+  osmextract::oe_get(place, query = query, extra_tags = et, ...)
+}
+
+oi_query = function(infra_type) {
+  preset_queries[[infra_type]]
+}
+
+# preset_extra_tags = c(
+et = c(
+  "maxspeed",
+  "ref",
+  "bicycle",
+  "cycleway",
+  "lanes",
+  "oneway",
+  "footway",
+  "access",
+  "sidewalk_left_bicycle",
+  "cycleway_left",
+  "cycleway_right",
+  "oneway_bicycle",
+  "sidewalk_right_bicycle",
+  "cycleway_both"
+)
+
+# could be a data object
+preset_queries = list(
+  highway = "select * from 'lines' where highway is not null",
+  cycle_infrastructure_minimal = "select * from 'lines' where (sidewalk_left_bicycle='yes') or
+ (cycleway='shared_busway') or
+ (cycleway='opposite_lane') or
+ (highway='bridleway' and bicycle='no') or
+ (bicycle='use_sidepath') or
+ (highway='path') or
+ (highway='path' and (bicycle='designated' or bicycle='official')) or
+ (highway='pedestrian' and (bicycle='yes' or bicycle='official')) or
+ (highway='footway') or
+ (highway='cycleway') or
+ (cycleway in ('lane', 'opposite_lane', 'shared_busway', 'track', 'opposite_track')) or
+ (cycleway='lane')
+ ",
+  cycle_infrastructure = "select * from 'lines' where (sidewalk_left_bicycle='yes') or
+ (cycleway_left='shared_lane') or
+ (cycleway_left='shared_busway') or
+ (cycleway_right='shared_busway') or
+ (cycleway='shared_busway') or
+ (cycleway='opposite_lane') or
+ (highway='bridleway' and bicycle='no') or
+ (bicycle='use_sidepath') or
+ (cycleway='opposite' and oneway_bicycle='no') or
+ (sidewalk_right_bicycle='yes') or
+ (cycleway_right='shared_lane') or
+ (cycleway_left='track') or
+ (cycleway_right='track') or
+ (highway='path') or
+ (highway='path' and (bicycle='designated' or bicycle='official')) or
+ (highway='pedestrian' and (bicycle='yes' or bicycle='official')) or
+ (highway='footway') or
+ (highway='cycleway') or
+ (cycleway in ('lane', 'opposite_lane', 'shared_busway', 'track', 'opposite_track')) or
+ (cycleway_left in ('lane', 'shared_busway')) or
+ (cycleway_right in ('lane', 'shared_busway')) or
+ (cycleway_both='lane') or
+ (cycleway='lane')
+ "
+  )
